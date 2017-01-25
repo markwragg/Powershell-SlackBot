49d9c257186aa84c45dce137833c9b4a2ec4a265
Mark Wragg <mark.wragg@gmail.com>
2017-01-25 13:50:54 +0000

    Add files via upload

---
+++ Default.Tests.ps1
@@ -0,0 +1,34 @@
+﻿# PSScriptAnalyzer tests
+$Scripts = Get-ChildItem “$PSScriptRoot\..\” -Filter ‘*.ps1’ -Recurse | Where-Object {$_.name -NotMatch ‘tests.ps1’ -and $_.name -NotMatch 'build.ps1'}
+$Modules = Get-ChildItem “$PSScriptRoot\..\” -Filter ‘*.psm1’ -Recurse
+$Rules   = Get-ScriptAnalyzerRule
+
+If ($Modules.count -gt 0) {
+  Describe ‘Testing all Modules against default PSScriptAnalyzer rule-set’ {
+    foreach ($module in $modules) {
+      Context “Testing Module '$($module.FullName)'” {
+        foreach ($rule in $rules) {
+          It “passes the PSScriptAnalyzer Rule $rule“ {
+            (Invoke-ScriptAnalyzer -Path $module.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
+          }
+        }
+      }
+    }
+  }
+}
+
+If ($Scripts.count -gt 0) {
+  Describe ‘Testing all Script against default PSScriptAnalyzer rule-set’ {
+    foreach ($Script in $scripts) {
+      Context “Testing Script '$($script.FullName)'” {
+        foreach ($rule in $rules) {
+          It “passes the PSScriptAnalyzer Rule $rule“ {
+            If (-not ($module.BaseName -match 'AppVeyor') -and -not ($rule.Rulename -eq 'PSAvoidUsingWriteHost') ) {
+              (Invoke-ScriptAnalyzer -Path $script.FullName -IncludeRule $rule.RuleName ).Count | Should Be 0
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ build.ps1
@@ -0,0 +1,148 @@
+Param(
+    [cmdletbinding()]
+    [string[]]$ModulesToPublish,
+    [string]$NuGetApiKey,
+    [switch]$Build,
+    [switch]$Install,
+    [switch]$Test,
+    [switch]$Deploy,
+    [switch]$InitialPublish
+)
+
+If ($env:ModulesToPublish)      { $ModulesToPublish = $env:ModulesToPublish }
+If ($ModulesToPublish)          { $ModulesToPublish = $ModulesToPublish.Split(',') }
+If ($env:NuGetApiKey)           { $NuGetApiKey = $env:NuGetApiKey }
+If ($Deploy -and !$NuGetApiKey) { Throw "-NuGetApiKey must be defined in order to deploy to PS Gallery" }
+
+If ($Build) {
+
+    If ($env:APPVEYOR){
+        Write-Host "Build Version    : $env:APPVEYOR_BUILD_VERSION"
+        Write-Host "Author           : $env:APPVEYOR_REPO_COMMIT_AUTHOR"
+        Write-Host "Branch           : $env:APPVEYOR_REPO_BRANCH"
+    }
+        Write-Host "ModulesToPublish : $ModulesToPublish"
+}
+
+
+If ($Install) {
+
+    Write-Host 'Installing NuGet Package Provider ..'
+    Install-PackageProvider -Name NuGet -Force | Out-Null
+    
+    Write-Host 'Installing Pester ..'
+    Install-Module -Name Pester -Repository PSGallery -Force
+
+    Write-Host 'Installing PSScriptAnalyzer ..'
+    Install-Module PSScriptAnalyzer -Repository PSGallery -force
+}
+
+
+If ($Test) {
+
+    $testResultsFile = 'TestsResults.xml'
+    $Results = Invoke-Pester -Script .\Tests\*.Tests.ps1 -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+
+    If ($env:APPVEYOR){
+        #----- Workaround for https://github.com/appveyor/ci/issues/1271 -----
+        [xml]$Content = Get-Content $testResultsFile
+        $Content.'test-results'.'test-suite'.type = "Powershell"
+        $Content.Save("$(Join-Path $pwd $testResultsFile)")
+        #---------------------------------------------------------------------
+        Write-Host "Uploading results to AppVeyor .."
+        
+        (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+    }
+
+    If (($Results.FailedCount -gt 0) -or ($Results.PassedCount -eq 0)) { 
+        Throw "$($Results.FailedCount) tests failed."
+    } Else {
+        Write-Host 'All tests passed.' -ForegroundColor Green
+    }
+}
+
+
+If ($Deploy) {
+    
+    If ($env:APPVEYOR -and $env:APPVEYOR_REPO_BRANCH -notmatch 'master') {
+        Write-Host "Finished testing of branch $env:APPVEYOR_REPO_BRANCH - Exiting." -ForegroundColor Green
+        Exit
+    }
+ 
+    If (!$ModulesToPublish) {
+        Write-Host "No modules are defined to be published - Exiting." -ForegroundColor Green
+        Exit
+    }
+
+    $ModulesToPublish | ForEach-Object {
+        
+        $ModuleManifest = Get-ChildItem "$pwd\$_\*.psd1"
+        
+        If ($ModuleManifest) {
+
+            $Module           = $ModuleManifest.BaseName
+            $ModulePath       = $ModuleManifest.Directory
+            $ManifestFullName = $ModuleManifest.FullName
+            $env:psmodulepath = $env:psmodulepath + ';' + $ModulePath
+            
+            If (!$InitialPublish) {
+                        
+                Write-Host "$Module : Checking module for differences with existing PowerShell Gallery version .."
+            
+                Try {
+                    Save-Module -Name $Module -Path .\ -ErrorAction Stop
+                    $ModuleContents    = Get-ChildItem -Exclude *.psd1 "$ModulePath\" | Where-Object { -not $_.PsIsContainer } -ErrorAction Stop | Get-Content
+                    $PSGModuleContents = Get-ChildItem -Exclude *.psd1 "$ModulePath\*\*" | Where-Object { -not $_.PsIsContainer } -ErrorAction Stop | Get-Content
+                } Catch {
+                    Throw "$Module : Could not get the contents of the local or Gallery module."
+                } Finally {
+                    Remove-Item "$ModulePath\*\*" -Recurse -Force
+                }
+
+                $Differences = Compare-Object $ModuleContents $PSGModuleContents
+
+                If ($Differences){
+            
+                    If (!$env:APPVEYOR_BUILD_VERSION) { 
+
+                        Try{
+                            Import-Module $ManifestFullName -Force -ErrorAction Stop
+
+                            $CurVersion = Get-Module $Module | Select-Object -ExpandProperty Version            
+                            $NewVersion = New-Object -TypeName System.Version -ArgumentList $CurVersion.Major, $CurVersion.Minor, ($CurVersion.Build + 1), 0
+                        } Catch {
+                            Throw "$Module : Could not establish new module version. Publish failed."
+                        }
+
+                    } Else { $NewVersion = $env:APPVEYOR_BUILD_VERSION }
+
+                    If (!$NuGetApiKey) { Throw "NuGetApiKey not specified. Cannot publish to PowerShell Gallery." }
+
+                    Write-Host "$Module : Updating module manifest to version $NewVersion .."            
+                        
+                    $ModuleManifest = Get-Content $ManifestFullName -Raw
+                    [regex]::replace($ModuleManifest,'(ModuleVersion = )(.*)',"`$1'$NewVersion'") | Out-File -LiteralPath $ManifestFullName
+               
+                    Write-Host "$Module : Publishing module to the PowerShell Gallery .."
+                
+                } Else {
+                    Write-Host "$Module : The module is already up to date in the Gallery - Exiting." -ForegroundColor Green
+                    Exit
+                }
+            }
+        
+            Try { 
+                Write-Host "$Module : Publishing module to the PowerShell Gallery .."   
+                Publish-Module -Path $ModulePath -NuGetApiKey $NuGetApiKey
+
+            } Catch { 
+                Throw "$Module : Failed to publish $ModulePath" 
+            }
+            
+        } Else {
+            Throw "$Module : Could not locate a module manifest in $pwd\$Module\"      
+        }
+        
+        Write-Host "$Module published successfully." -ForegroundColor Green
+    }
+}
