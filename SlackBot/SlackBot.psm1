3e84ca83897f2f0d274e0330bdcb8a3d672d7ea8
Mark Wragg <mark@wragg.io>
2017-03-23 11:19:01 +0000

    Removed cmdlet alias
    Was failing script analyzer due to use of % alias, replaced with ForEach-Object.

--- SlackBot/SlackBot.psm1
+++ SlackBot/SlackBot.psm1
@@ -1,17 +1,13 @@
 $Public = @( Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" )
 $Private = @( Get-ChildItem -Path "$PSScriptRoot\Private\*.ps1" )
 
-@($Public + $Private) | %{
-    Try
-    {
+@($Public + $Private) | ForEach-Object {
+    Try {
         . $_.FullName
-    }
-    Catch
-    {
+    } Catch {
         Write-Error -Message "Failed to import function $($_.FullName): $_"
     }
 }
 
 Export-ModuleMember -Function $Public.BaseName
-
 Export-ModuleMember -Variable 'LogPath'
