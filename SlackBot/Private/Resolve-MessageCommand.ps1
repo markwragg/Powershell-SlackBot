6ae04e49f6f14a3d557ac446bd9539299960b3e2
Chirishman <Chirishman@users.noreply.github.com>
2017-03-27 19:59:02 -0400

    Add files via upload

---
+++ SlackBot/Private/Resolve-MessageCommand.ps1
@@ -0,0 +1,25 @@
+﻿function Resolve-MessageCommand {
+    Param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$BotName,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+            [ValidateNotNullOrEmpty()]
+            [string]$message,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+            [ValidateNotNullOrEmpty()]
+            $Channel
+    )
+
+    $words = "$($message)".ToLower()
+    while ($words -match "  "){
+        $words = $words -replace "  "," "
+    }
+    $words = $words -split " " | ?{$_}
+
+    
+    & (Get-StoredBot -BotName $BotName)
+
+    
+
+}
